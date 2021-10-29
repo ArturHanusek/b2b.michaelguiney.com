cfdb7dbe76b4d14b5f5e74a5a0bdac86c2326cac
Artur Hanusek <artur@arturhanusek.com>
2021-10-29 17:01:39 +0200

    change redirect

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo('ERROR: Store not found');
+//echo('ERROR: Store not found');
 // index.php
-//header("Location: https://guineys.ie");
+header("Location: https://b2b.michaelguiney.com");
 exit();
